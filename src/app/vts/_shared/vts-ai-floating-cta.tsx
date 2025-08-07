46986dba5d9fcfa544247540ae2ff731b4f4cb38
David Chan <david.chan@vts.com>
2025-08-07 14:55:57 -0400

    Remove console log from VtsAiFloatingCTA component to clean up code and improve performance

--- src/app/vts/_shared/vts-ai-floating-cta.tsx
+++ src/app/vts/_shared/vts-ai-floating-cta.tsx
@@ -67,7 +67,6 @@ export default function VtsAiFloatingCTA({ className }: { className?: string }) 
     if (currentPrompt) {
       setVtsAiContentType("default", currentPrompt.payload);
       setIsVtsAiOpen(true);
-      console.log(lottieRef.current);
       lottieRef.current?.playSegments([15, 30], true);
     }
   };
